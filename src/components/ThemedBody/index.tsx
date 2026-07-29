3361f519f147a2bf520850d7f1fb1072721e90c2
John Chantler <114353216+johnchantler@users.noreply.github.com>
2026-07-29 10:14:36 +0200

    Refactor Font Source

--- src/components/ThemedBody/index.tsx
+++ src/components/ThemedBody/index.tsx
@@ -155,14 +155,6 @@ export const ThemedBody = async ({ children }: { children: ReactNode }) => {
                 font-style: normal;
                 font-display: swap;
               }
-
-               @font-face {
-                font-family: 'content';
-                src: url('content.woff2') format('woff2');
-                font-weight: normal;
-                font-style: normal;
-                font-display: swap;
-              }
             `}
           </style>
         )}
