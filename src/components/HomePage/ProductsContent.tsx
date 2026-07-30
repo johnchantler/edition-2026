fdaa636441de021d31e484554935d26cce861281
John Chantler <114353216+johnchantler@users.noreply.github.com>
2026-07-30 20:19:28 +0200

    Single Column of releases on mobile. 'See All' link re-formatting.

--- src/components/HomePage/ProductsContent.tsx
+++ src/components/HomePage/ProductsContent.tsx
@@ -20,7 +20,7 @@ export async function ProductsContent() {
 
   return (
     <section className="py-20">
-      <div className="grid grid-cols-2 gap-8 pb-20 sm:max-w-full sm:grid-cols-2 xl:grid-cols-2">
+      <div className="grid grid-cols-1 gap-8 pb-20 sm:max-w-full sm:grid-cols-2 xl:grid-cols-2">
         {topProducts?.length
           ? topProducts.map((product) => (
               <ListProduct
@@ -39,14 +39,12 @@ export async function ProductsContent() {
           ))}
         </div>
       ) : null}
-      <div className="w-full grid-cols-3 sm:grid">
-        <span></span>
-        <span></span>
+      <div className="w-full grid-cols-3">
         <Link
-          className="flex w-full hover:translate-y-0.5 hover:text-secondary sm:relative sm:flex-row"
+          className="flex w-full justify-center font-content text-sm text-highlight hover:translate-y-0.5 hover:text-secondary sm:relative sm:flex-row"
           href="/shop"
         >
-          → see all
+          [ see all fönstret releases ]
         </Link>
       </div>
     </section>
